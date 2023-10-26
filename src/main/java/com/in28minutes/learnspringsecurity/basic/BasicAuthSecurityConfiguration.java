05936a40ee035fab62d609a78eae829f5a0ce160
Beginner <dkstmdwn17@naver.com>
2023-10-26 23:56:07 +0900

    JDBC를 이요하여 인증해보기(Embedded H2db)

--- src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -1,13 +1,17 @@
 package com.in28minutes.learnspringsecurity.basic;
 
+import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
+
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,10 +26,11 @@ public class BasicAuthSecurityConfiguration {
     });
     // http.formLogin(); -> form 로그인 방식을 사용하지 않음
     http.sessionManagement(
-      session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않음
+        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않음
     );
     http.httpBasic();
     http.csrf().disable();
+    http.headers().frameOptions().sameOrigin(); // 요청이 동일한 오리진에서 오는 경우 해당 애플리케이션에 대한 프레임을 허용하도록 지정
     return http.build();
   }
 
@@ -43,12 +48,12 @@ public class BasicAuthSecurityConfiguration {
       }
     };
   }
-  
+
   // UserDetailsService 를 설정
   // 사용자 세부 정보를 가져올 때 이 인터페이스를 사용하게 됨
   // Core interface which loads user-specific data.
   // InMemoryUserDetailsManager 는 UserDetailsManager 의 비지속적 구현 (휘발성이라고 생각하면 될듯)
-  @Bean
+/*  @Bean
   public UserDetailsService userDetailsService() { // 메모리에 유저 정보를 넣어줌 (휘발성) -> 운영에서는 권장X
 
     var user = User.withUsername("in28minutes") // User 객체를 생성하는 방법
@@ -62,6 +67,34 @@ public class BasicAuthSecurityConfiguration {
         .build();
 
     return new InMemoryUserDetailsManager(user, admin);
+  }*/
+
+  @Bean
+  public UserDetailsService userDetailsService(
+      DataSource dataSource) { // DataSource를 주입해준다.
+
+    var user = User.withUsername("in28minutes") // User 객체를 생성하는 방법
+        .password("{noop}dummy")
+        .roles(Role.USER.name()) // Enum 으로 넣어주자
+        .build();
+
+    var admin = User.withUsername("admin")
+        .password("{noop}dummy")
+        .roles(Role.ADMIN.name())
+        .build();
+    // InMemoryUserDetailsManager -> JdbcUserDetailsManager
+    var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+    jdbcUserDetailsManager.createUser(user);
+    jdbcUserDetailsManager.createUser(admin);
+    return jdbcUserDetailsManager;
+  }
+
+  @Bean
+  public DataSource dataSource () {
+    return new EmbeddedDatabaseBuilder()
+        .setType(EmbeddedDatabaseType.H2) // EmbeddedDatabaseType -> DERBY, H2, HSQL
+        .addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION) // JdbcDaoImpl 에 있는 ddl Script를 가지고 와서 사용할 예정
+        .build();
   }
 }
 
