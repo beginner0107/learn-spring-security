ddbb40920aa35fd79ab9092dc86f97d634dbfe94
Beginner <dkstmdwn17@naver.com>
2023-10-28 01:19:31 +0900

    JWT 인증 설정하기 2 - RSA Key 생성하기

--- src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtSecurityConfiguration.java
@@ -3,6 +3,19 @@ package com.in28minutes.learnspringsecurity.jwt;
 import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
 import com.in28minutes.learnspringsecurity.basic.Role;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.UUID;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +26,8 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -78,6 +93,42 @@ public class JwtSecurityConfiguration {
     return new EmbeddedDatabaseBuilder()
         .setType(EmbeddedDatabaseType.H2) // EmbeddedDatabaseType -> DERBY, H2, HSQL
         .addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION) // JdbcDaoImpl 에 있는 ddl Script를 가지고 와서 사용할 예정
+        .build();
+  }
+
+  // RSA키 만들기 Create Key Pair
+  // use java.security.KeyPairGenerator
+  // can use openssl as well
+  @Bean
+  public KeyPair keyPair() {
+    try {
+      var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+      keyPairGenerator.initialize(2048);
+      return keyPairGenerator.generateKeyPair();
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  // Create RSA Key object using Key Pair
+  @Bean
+  public RSAKey rsaKey() {
+    return new RSAKey.Builder((RSAPublicKey) keyPair().getPublic()) // 공개키
+        .privateKey(keyPair().getPrivate()) // 개인키
+        .keyID(UUID.randomUUID().toString())
+        .build();
+  }
+
+  @Bean
+  public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+    var jwkSet = new JWKSet(rsaKey);
+    return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() throws JOSEException {
+    return NimbusJwtDecoder
+        .withPublicKey(rsaKey().toRSAPublicKey())
         .build();
   }
 }
